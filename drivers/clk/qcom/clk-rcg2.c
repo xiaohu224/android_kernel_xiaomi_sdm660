221eaa52a4fd5f054a0af891d1955495623cd7d5
xiaohu224 <xiaohuluobo2240@outlook.com>
2026-02-10 01:29:06 +0800

    Revert "clk: qcom: clk-rcg2: Fix clock rate overflow for high parent frequencies"
    This reverts commit 2eae9e89095bc9de66aa400fd02c1b5a94290407.

--- drivers/clk/qcom/clk-rcg2.c
+++ drivers/clk/qcom/clk-rcg2.c
@@ -239,11 +239,17 @@ static void disable_unprepare_rcg_srcs(struct clk *curr, struct clk *new)
 static unsigned long
 calc_rate(unsigned long rate, u32 m, u32 n, u32 mode, u32 hid_div)
 {
-	if (hid_div)
-		rate = mult_frac(rate, 2, hid_div + 1);
+	u64 tmp = rate;
 
-	if (mode)
-		rate = mult_frac(rate, m, n);
+	if (hid_div) {
+		tmp *= 2;
+		do_div(tmp, hid_div + 1);
+	}
+
+	if (mode) {
+		tmp *= m;
+		do_div(tmp, n);
+	}
 
 	return tmp;
 }
