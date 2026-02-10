b744547a0ca88ff46b11c5cb1926f02962f085b7
xiaohu224 <xiaohuluobo2240@outlook.com>
2026-02-11 01:13:17 +0800

    Revert "timers: Update kernel-doc for various functions"
    This reverts commit 42f1b09beb4b1992082e7022e5c5b878ed34f4dd.

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1234,7 +1234,7 @@ EXPORT_SYMBOL_GPL(add_timer_on);
  * @timer:	The timer to be deactivated
  *
  * The function only deactivates a pending timer, but contrary to
- * del_timer_sync() it does not take into account whether the timer's
+ * timer_delete_sync() it does not take into account whether the timer's
  * callback function is concurrently executed on a different CPU or not.
  * It neither prevents rearming of the timer. If @timer can be rearmed
  * concurrently then the return value of this function is meaningless.
@@ -1368,8 +1368,9 @@ static inline void del_timer_wait_running(struct timer_list *timer) { }
 #endif
 
 #if defined(CONFIG_SMP) || defined(CONFIG_PREEMPT_RT)
-/*
- * The counterpart to del_timer_wait_running().
+/**
+ * del_timer_sync - deactivate a timer and wait for the handler to finish.
+ * @timer: the timer to be deactivated
  *
  * If there is a waiter for base->expiry_lock, then it was waiting for the
  * timer callback to finish. Drop expiry_lock and reaquire it. That allows
@@ -1449,7 +1450,7 @@ static inline void del_timer_wait_running(struct timer_list *timer) { }
  *    timer_delete_sync(mytimer);
  *    while (base->running_timer == mytimer);
  *
- * Now del_timer_sync() will never return and never release somelock.
+ * Now timer_delete_sync() will never return and never release somelock.
  * The interrupt on the other CPU is waiting to grab somelock but it has
  * interrupted the softirq that CPU0 is waiting to finish.
  *
